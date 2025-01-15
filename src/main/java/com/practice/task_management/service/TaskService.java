43fbcb9bf3ecd08a53dd3f42abe9c642b7464e53
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-01-15 12:53:38 +0300

    Made TaskService

--- src/main/java/com/practice/task_management/service/TaskService.java
+++ src/main/java/com/practice/task_management/service/TaskService.java
@@ -2,10 +2,12 @@ package com.practice.task_management.service;
 
 import com.practice.task_management.dto.TaskDto;
 import com.practice.task_management.exception.TaskNotFoundException;
-import com.practice.task_management.model.Priority;
-import com.practice.task_management.model.Task;
-import com.practice.task_management.model.TaskStatus;
+import com.practice.task_management.model.*;
+import com.practice.task_management.repository.BacklogRepository;
+import com.practice.task_management.repository.ProjectRepository;
 import com.practice.task_management.repository.TaskRepository;
+import com.practice.task_management.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,77 +16,90 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class TaskService {
+
     private final TaskRepository taskRepository;
+    private final BacklogRepository backlogRepository;
+    private final ProjectRepository projectRepository;
+    private final UserRepository userRepository;
+    private final ProjectService projectService;
 
-    public TaskService(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
-    }
+    public Task addProjectTask(String projectIdentifier, Task projectTask, String userEmail, String username) {
+        Backlog backlog = projectService.findProjectByIdentifier(projectIdentifier, username).getBacklog();
 
-    public TaskDto createTask(TaskDto taskDto, String userEmail) {
+        projectTask.setBacklog(backlog);
 
-        if (taskDto.getTitle() == null || taskDto.getTitle().isEmpty()) {
-            throw new TaskNotFoundException.InvalidRequestException("Task title cannot be empty");
+        User user = userRepository.findByEmail(userEmail);
+        projectTask.setUser(user);
+
+        projectTask.setAssignBy(username);
+
+        Integer backLogSequence = backlog.getSequence();
+        backLogSequence++;
+        backlog.setSequence(backLogSequence);
+
+        projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + backLogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO DO");
         }
 
-        Task task = new Task();
-        task.setTitle(taskDto.getTitle());
-        task.setDescription(taskDto.getDescription());
-        task.setStatus(TaskStatus.valueOf(taskDto.getStatus()));
-        task.setPriority(Priority.valueOf(taskDto.getPriority()));
-        taskRepository.save(task);
-        return mapToDto(task);
+        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+            projectTask.setPriority(3);
+        }
+
+        return taskRepository.save(projectTask);
     }
 
-    public List<TaskDto> getTasks(String userEmail) {
-        return taskRepository.findByUserEmail(userEmail).stream()
-                .map(this::mapToDto)
-                .collect(Collectors.toList());
+    public Iterable<Task> findBacklogById(String backlogId, String username) {
+        projectService.findProjectByWorker(backlogId, username);
+        return taskRepository.findByProjectIdentifier(backlogId);
     }
 
+    public Task findPTByProjectSequence(String backlog_id, String pt_id, String username) {
+        projectService.findProjectByIdentifier(backlog_id, username);
 
-    public TaskDto getTaskById(Long id) {
-        Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException("Task not found"));
-        return mapToDto(task);
+        Task task = taskRepository.findByProjectSequence(pt_id);
+        if (task == null) {
+            throw new ProjectNotFoundException("Task " + pt_id + "was not found");
+        }
+
+        if (!task.getProjectIdentifier().equals(backlog_id)) {
+            throw new ProjectNotFoundException("Task " + pt_id + "does not exist");
+        }
+        return task;
     }
 
-    public TaskDto updateTask(Long id, TaskDto taskDto) {
-        Task task = taskRepository.findById(id).orElseThrow(() -> new TaskNotFoundException("Task not found"));
-        task.setTitle(taskDto.getTitle());
-        task.setDescription(taskDto.getDescription());
-        task.setStatus(TaskStatus.valueOf(taskDto.getStatus()));
-        task.setPriority(Priority.valueOf(taskDto.getPriority()));
-        taskRepository.save(task);
-        return mapToDto(task);
+    public Task findTaskBySequence(String projectSequence, String username) {
+        Task task = taskRepository.findByProjectSequence(projectSequence);
+        projectService.findProjectByWorker(task.getProjectIdentifier(), username);
+        return task;
     }
 
-    public void deleteTask(Long id) {
-        taskRepository.deleteById(id);
+    public Task updateByProjectSequence(Task updateTask, String backlog_id, String pt_id, String username) {
+        Task task = findPTByProjectSequence(backlog_id, pt_id, username);
+        task = updateTask;
+        return taskRepository.save(task);
     }
 
-    public Page<TaskDto> getTasksWithFilters(String status, String priority, String title, Pageable pageable) {
-        return taskRepository.findAllByFilters(status, priority, title, pageable)
-                .map(this::mapToDto);
+    public void deletePTByProjectSequence(String backlog_id, String pt_id, String username) {
+        Task projectTask = findPTByProjectSequence(backlog_id, pt_id, username);
+        taskRepository.delete(projectTask);
     }
 
-    private TaskDto mapToDto(Task task) {
-        TaskDto dto = new TaskDto();
-        dto.setId(task.getId());
-        dto.setTitle(task.getTitle());
-        dto.setDescription(task.getDescription());
-        dto.setStatus(task.getStatus().name());
-        dto.setPriority(task.getPriority().name());
-        return dto;
+    public Iterable<Task> findAllTaskByUser(String username) {
+        return taskRepository.findByUserEmail(username);
     }
 
+    public Task updateStatus(String task_id, Integer status, String username) {
+        Task task = taskRepository.findByProjectSequence(task_id);
 
-//    private TaskDto mapToDto(Task task) {
-//        TaskDto dto = new TaskDto();
-//        dto.setId(task.getId());
-//        dto.setTitle(task.getTitle());
-//        dto.setDescription(task.getDescription());
-//        dto.setStatus(String.valueOf(TaskStatus.valueOf(dto.getStatus())));
-//        dto.setPriority(String.valueOf(Priority.valueOf(dto.getPriority())));
-//        return dto;
-//    }
+        if (!task.getUser().getEmail().equals(username)) {
+            throw new TaskNotFoundException("Task " + task_id + "not found");
+        }
+        task.setStatus(status == 2 ? "PROGRESS" : "COMPLETED");
+        return taskRepository.save(task);
+    }
 }
