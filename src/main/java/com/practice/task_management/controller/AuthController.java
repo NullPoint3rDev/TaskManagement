7091b667a74a5d07c4a3a1cd7a004f886297818d
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-01-15 10:52:30 +0300

    Some changes in AuthController

--- src/main/java/com/practice/task_management/controller/AuthController.java
+++ src/main/java/com/practice/task_management/controller/AuthController.java
@@ -3,32 +3,40 @@ package com.practice.task_management.controller;
 import com.practice.task_management.dto.AuthRequest;
 import com.practice.task_management.dto.AuthResponse;
 import com.practice.task_management.dto.RegisterRequest;
+import com.practice.task_management.model.User;
 import com.practice.task_management.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 @RestController
-@RequestMapping("/auth")
 public class AuthController {
 
-    private final AuthService authService;
+    private final UserService userService;
 
-    public AuthController(AuthService authService) {
-        this.authService = authService;
-    }
+    private final ValidationErrorService validationErrorService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-        authService.register(request);
-        return ResponseEntity.ok("User registered successfully!");
+    public ResponseEntity<?> register(@Valid @RequestBody User user BindingResult result) {
+        ResponseEntity<?> errorMap = validationErrorService.ValidationService(result);
+
+        if (errorMap != null) return errorMap;
+
+        User newUser = userService.register(user);
+        return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        return ResponseEntity.ok(authService.authenticate(request));
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(userService.login(request));
     }
 
 }
