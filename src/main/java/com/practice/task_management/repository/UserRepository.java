277b162fa86d0f8e887774932c59c1ac5213d60e
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-01-15 10:36:09 +0300

    Updated UserRepository interface

--- src/main/java/com/practice/task_management/repository/UserRepository.java
+++ src/main/java/com/practice/task_management/repository/UserRepository.java
@@ -3,8 +3,13 @@ package com.practice.task_management.repository;
 import com.practice.task_management.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByEmail(String email);
+
+    User getById(Integer id);
+
+    Iterable<User> findByEmailNot(String email);
 }
