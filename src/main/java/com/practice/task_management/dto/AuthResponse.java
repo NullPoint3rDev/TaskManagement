10e1c13597610db4bea5f6cb48986eebd947f33d
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-01-15 14:32:29 +0300

    Some changes in AuthResponse

--- src/main/java/com/practice/task_management/dto/AuthResponse.java
+++ src/main/java/com/practice/task_management/dto/AuthResponse.java
@@ -1,13 +1,15 @@
 package com.practice.task_management.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String token;
-
-    public AuthResponse(String token) {
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
 }
