5ec6e44c0f0654f99c8e9a4d9cc47d510042b083
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-01-15 14:35:34 +0300

    Some changes in RegisterRequest

--- src/main/java/com/practice/task_management/dto/RegisterRequest.java
+++ src/main/java/com/practice/task_management/dto/RegisterRequest.java
@@ -1,22 +1,23 @@
 package com.practice.task_management.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "We need your name")
+    private String name;
+
+    @NotBlank(message = "We need an email")
     private String email;
-    private String password;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @NotBlank(message = "We need a password")
+    private String
 }
